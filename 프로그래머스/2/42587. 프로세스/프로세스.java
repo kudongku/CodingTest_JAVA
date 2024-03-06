d6afaaaba7b686724553d8f93508a6e4f9958ed2
구동현 <148612321+kudongku@users.noreply.github.com>
2024-03-06 09:49:16 +0900

    [level 2] Title: 프로세스, Time: 0.58 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -5,7 +5,6 @@ import java.util.Queue;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        int l = location;
 
         Queue<Integer> que = new LinkedList<Integer>();
         for(int i : priorities){
@@ -15,20 +14,18 @@ class Solution {
         Arrays.sort(priorities);
         int size = priorities.length-1;
 
-
-
         while(!que.isEmpty()){
             Integer i = que.poll();
             if(i == priorities[size - answer]){
                 answer++;
-                l--;
-                if(l <0)
+                location--;
+                if(location<0)
                     break;
             }else{
                 que.add(i);
-                l--;
-                if(l<0)
-                    l=que.size()-1;
+                location--;
+                if(location<0)
+                    location=que.size()-1;
             }
         }
 
