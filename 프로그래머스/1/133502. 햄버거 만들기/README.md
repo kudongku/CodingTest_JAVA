7c03f0349c4425b829e1259bdd1b0425fb6ef148
구동현 <148612321+kudongku@users.noreply.github.com>
2024-03-08 09:02:17 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.15 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
-메모리: 71.7 MB, 시간: 0.14 ms
+메모리: 74.9 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 9:58:4
+2024년 03월 08일 09:02:15
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -5,11 +5,11 @@ class Solution {
         int answer = 0;
 
         Stack<Integer> stack = new Stack<>();
-        
-        for(int i=0; i<ingredient.length; i++){
-            stack.add(ingredient[i]);
-            
-            if(stack.size()>3 && stack.peek()==1 && stack.get(stack.size()-2)==3 && stack.get(stack.size()-3)==2 && stack.get(stack.size()-4)==1){
+
+        for (int j : ingredient) {
+            stack.add(j);
+
+            if (stack.size() > 3 && stack.peek() == 1 && stack.get(stack.size() - 2) == 3 && stack.get(stack.size() - 3) == 2 && stack.get(stack.size() - 4) == 1) {
                 stack.pop();
                 stack.pop();
                 stack.pop();
