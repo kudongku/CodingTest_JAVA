9ec823580e2f3af2450da38737567f2edf91d49c
구동현 <148612321+kudongku@users.noreply.github.com>
2024-03-12 09:13:38 +0900

    [level 1] Title: 둘만의 암호, Time: 0.60 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,22 +1,19 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.ArrayList;
-
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder answer = new StringBuilder();
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-        for(int i=0; i<skip.length(); i++){
-            if(alphabet.indexOf(skip.charAt(i))!=-1){
-                alphabet = alphabet.replaceAll(String.valueOf(skip.charAt(i)), "");
+        for (int i = 0; i < skip.length(); i++) {
+            if (alphabet.indexOf(skip.charAt(i)) != -1) {
+                alphabet = alphabet.replaceAll(String.valueOf(skip.charAt(i)), " ");
             }
         }
 
+        alphabet = alphabet.replaceAll(" ", "");
+
         alphabet = alphabet.repeat(23);
 
-        for(int i=0; i<s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char target = s.charAt(i);
             int targetIdx = alphabet.indexOf(target);
             answer.append(alphabet.charAt(targetIdx + index));
