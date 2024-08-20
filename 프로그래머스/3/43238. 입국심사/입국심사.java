652d7b23cf9a579170c25fd397984a9a42974031
구동현 <148612321+kudongku@users.noreply.github.com>
2024-08-20 18:28:17 +0900

    [level 3] Title: 입국심사, Time: 83.80 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,27 +3,27 @@ import java.util.Arrays;
 class Solution {
 
     public long solution(int n, int[] times) {
-        long left = 1; // 최소 시간
-        long right = Arrays.stream(times).max().getAsInt() * (long) n; // 최대 시간
-        long answer = right;
+        long left = 1;
+        long right = (long) Arrays.stream(times).max().getAsInt() * n;
+        long minutes = right;
 
         while (left <= right) {
+            long count = 0;
             long mid = (left + right) / 2;
-            long peopleProcessed = 0;
 
-            // 주어진 중간 시간 내에 처리할 수 있는 사람 수 계산
-            for (int time : times) {
-                peopleProcessed += mid / time;
+            for (int i : times) {
+                count += mid / i;
             }
 
-            if (peopleProcessed >= n) {
-                answer = mid; // 모든 사람을 처리할 수 있다면 시간을 줄여서 더 좋은 해를 찾는다
+            if (count >= n) {
+                minutes = mid;
                 right = mid - 1;
-            } else {
-                left = mid + 1; // 모든 사람을 처리할 수 없다면 시간을 늘린다
+            }else{
+                left = mid + 1;
             }
+
         }
 
-        return answer;
+        return minutes;
     }
 }
